c0579920f6eff081438e4c7bd093581db8bb911d
NaqibKhusairy <naaqibkhusairy@gmail.com>
2023-08-26 21:11:36 +0800

    Done first page

--- app/src/main/java/com/naqib/food_ordering3_sqlite/MainActivity.java
+++ app/src/main/java/com/naqib/food_ordering3_sqlite/MainActivity.java
@@ -2,13 +2,35 @@ package com.naqib.food_ordering3_sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button order, viewhistory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        order = findViewById(R.id.btnorder);
+        viewhistory = findViewById(R.id.btnhistory);
+
+        order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent x = new Intent(getApplicationContext(),OrderActivity.class);
+                startActivity(x);
+            }
+        });
+
+        viewhistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent x = new Intent(getApplicationContext(),HistoryActivity.class);
+                startActivity(x);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/naqib/food_ordering3_sqlite/OrderActivity.java
@@ -0,0 +1,14 @@
+package com.naqib.food_ordering3_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
